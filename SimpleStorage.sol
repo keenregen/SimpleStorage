7f6790555df2327f4b6f46bcec091f8b08658980
keenregen <keenregen@gmail.com>
2023-01-26 17:42:40 +0300

    com1.1

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Blank  
-// Warnings does not stop you from compiling but errors does
+// Warnings does not stop you from compiling but errors do
 
 pragma solidity ^0.8.8;
 // pragma solidity 0.8.7;
